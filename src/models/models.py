29cfb435dc6928d507b4ace14b609ddaf0a503f4
ahmadzip <ahmad.5220411084@student.uty.ac.id>
2024-01-09 21:01:04 +0700

    Mengkoneksikan ke model database pemasukan

--- src/models/models.py
+++ src/models/models.py
@@ -29,21 +29,22 @@ class Pemasukan(db.Model):
     __tablename__ = "pemasukan"
     id = db.Column(db.Integer, primary_key=True)
     tanggal = db.Column(db.DateTime, nullable=False)
+    # Use 'keterangan' here
     keterangan = db.Column(db.String(255), nullable=False)
-    jumlah = db.Column(db.Integer, nullable=False)
+    nominal = db.Column(db.Integer, nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     user = db.relationship("User", backref=db.backref("pemasukan", lazy=True))
 
-    def __init__(self, tanggal, keterangan, jumlah, user_id):
+    def __init__(self, tanggal, keterangan, nominal, user_id):
         self.tanggal = tanggal
         self.keterangan = keterangan
-        self.jumlah = jumlah
+        self.nominal = nominal
         self.user_id = user_id
         self.created_on = datetime.now()
 
     def __repr__(self):
-        return f"<keterangan {self.keterangan}>"
+        return f"<id {self.id}>"
 
 
 class Pengeluaran(db.Model):
